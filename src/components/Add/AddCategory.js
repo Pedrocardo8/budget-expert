b67f7cdc5ee931b5211acaf25eed6526e3444178
João Brito <joaomanuelbrito@hotmail.com>
2021-05-27 15:07:53 +0100

    Revê valores da categoria antes de enviar para a bd, mostra span em caso de erro

--- src/components/Add/AddCategory.js
+++ src/components/Add/AddCategory.js
@@ -1,20 +1,47 @@
-import React , { useState, useContext } from 'react';
+import React , { useState, useEffect } from 'react';
 import { Form, Button } from "react-bootstrap"
 import { useAuth } from '../../context/AuthContext'
 import firebase from '../../firebase';
 
 
     function AddCategory(){
+        // criar variáveis para usar no controlo
         const { currentUser, logout } = useAuth()
         const [category, setCategory] = useState('');
+        const [categories, setCategories] = useState([]);
+        const [categoryCreate, setCategoryCreate] = useState(true);
+
+        // vai buscar todas as categorias adicionadas anteriormente
+        useEffect(() => {
+            var user = firebase.auth().currentUser;
+            const ref = firebase.firestore().collection("categories").where("userId", "==", user.uid)
+            ref.onSnapshot((querySnapchot) => {
+                const items = [];
+                querySnapchot.forEach((doc) => {
+                    items.push(doc.data())
+                });
+                setCategories(items);
+            })
+        }, []);
+
+
+
+        // adicionar nova categoria
         function addNewCategory(e){
             e.preventDefault()
-            const db = firebase.firestore()
-            db.collection("categories").add({
-                titulo:category,
-                userId: currentUser.uid
-            })
-            setCategory('')
+            setCategoryCreate(true);
+            for(let i = 0; i < categories.length; i++) {
+                if(categories[i].titulo == category){setCategoryCreate(false)}
+            };
+            if(categoryCreate){
+                const db = firebase.firestore()
+                db.collection("categories").add({
+                    titulo:category,
+                    userId: currentUser.uid
+                })
+                // repor valor inicial dos campos
+                setCategory('')
+            }
     }
 
     return(
@@ -24,6 +51,7 @@ import firebase from '../../firebase';
                 <Form.Group controlId="exampleForm.ControlTextarea1">
                     <Form.Control as="textarea" value={category} onChange={(e) => setCategory(e.target.value)}></Form.Control>
                 </Form.Group>
+                <span style={{ display: categoryCreate ? "none" : "block"}} className="error">Category already exists</span>
                 <Button variant="primary" type="submit" block >Add</Button>                                        
             </Form>
         </div>
